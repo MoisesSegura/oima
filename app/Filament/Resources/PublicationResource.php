fb1129124690b1701948272721c9ac2233e3a071
Gustavo Murillo <murillex1@hotmail.com>
2023-09-18 23:04:34 -0600

    add files

---
+++ app/Filament/Resources/PublicationResource.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PublicationResource\Pages;
+use App\Filament\Resources\PublicationResource\RelationManagers;
+use App\Models\Publication;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+
+class PublicationResource extends Resource
+{
+    protected static ?string $model = Publication::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make()
+                ->schema([
+                    Forms\Components\TextInput::make('author')
+                        ->required()
+                        ->live(onBlur: true)
+                        ->afterStateUpdated(fn (string $operation, $state, Forms\Set $set) => $operation === 'create' ? $set('slug', Str::slug($state)) : null),
+
+                    Forms\Components\TextInput::make('place')
+                        ->required()
+                        ->live(onBlur: true)
+                        ->afterStateUpdated(fn (string $operation, $state, Forms\Set $set) => $operation === 'create' ? $set('slug', Str::slug($state)) : null),
+
+                    Forms\Components\TextInput::make('isbn')
+                        ->required()
+                        ->live(onBlur: true)
+                        ->afterStateUpdated(fn (string $operation, $state, Forms\Set $set) => $operation === 'create' ? $set('slug', Str::slug($state)) : null),
+
+                    Forms\Components\TextInput::make('topographic_signature')
+                        ->disabled()
+                        ->dehydrated()
+                        ->required(),
+
+
+                ])
+                ->columns(2),
+                
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('author')
+                    ->searchable()
+                    ->sortable(),
+                    Tables\Columns\TextColumn::make('topographic_signature')
+                    ->searchable()
+                    ->sortable(),                    
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+
+                Tables\Actions\EditAction::make(),
+
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPublications::route('/'),
+            'create' => Pages\CreatePublication::route('/create'),
+            'edit' => Pages\EditPublication::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ database/migrations/2023_09_12_221058_create_permission_tables.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Spatie\Permission\PermissionRegistrar;
+
+class CreatePermissionTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $tableNames = config('permission.table_names');
+
+        if (empty($tableNames)) {
+            throw new \Exception('Error: config/permission.php not found and defaults could not be merged. Please publish the package configuration before proceeding, or drop the tables manually.');
+        }
+
+        Schema::drop($tableNames['role_has_permissions']);
+        Schema::drop($tableNames['model_has_roles']);
+        Schema::drop($tableNames['model_has_permissions']);
+        Schema::drop($tableNames['roles']);
+        Schema::drop($tableNames['permissions']);
+    }
+}
